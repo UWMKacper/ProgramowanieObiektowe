a8cc9acfd39f6addf185f151762a2c1e94b9b71c
Kacper <165946@student.edu.pl>
2022-10-13 21:57:40 +0200

    Zadania z rozdziału 2.

---
+++ Rozdzial2/Rozdzial2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ProgramowanieObiektowe
+{
+    class Rozdzial2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zad 1:\n");
+            Rozdzial2.Zadanie1();
+            Console.WriteLine("\nZad 2:\n");
+            Rozdzial2.Zadanie2();
+            Console.WriteLine("\nZad 3:\n");
+            Rozdzial2.Zadanie3();
+        }
+
+        static void Zadanie1()
+        {
+            /*Napisz program przeliczający temperaturę w stopniach Celsjusza na temperaturę w 
+             * stopniach Fahrenheita. Program ma prosić użytkownika o podanie temperatury w stopniach
+             * Celsjusza.*/
+
+            float tempInCelsius, tempInFarenheit;
+
+            Console.WriteLine("Podaj wartość temperatury w C: ");
+            tempInCelsius = float.Parse(Console.ReadLine());
+            tempInFarenheit = 32 + ((float)9 / 5) * tempInCelsius;
+            Console.WriteLine("{0:F2}C to {1:F2}F.", tempInCelsius, tempInFarenheit);
+        }
+
+        static void Zadanie2()
+        {
+            /*Napisz program, który oblicza deltę dla równania kwadratowego ax^2 + b + c = 0. Program ma
+             * prosić użytkownika o podanie współczynników równania a, b oraz c.*/
+
+            int a, b, c, delta;
+
+            Console.WriteLine("Podaj wartość a (int): ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj wartość b (int): ");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj wartość c (int): ");
+            c = int.Parse(Console.ReadLine());
+
+            delta = (int)Math.Pow(b, 2) - 4 * a * c;
+            Console.WriteLine(delta);
+        }
+
+        static void Zadanie3()
+        {
+            /*Napisz program, który oblicza wskaźnik masy ciała BMI. Program ma prosić użytkownika o 
+             * podanie wagi w kilogramach oraz wzrostu w metrach.*/
+
+            float masa, wzrost, bmi;
+
+            Console.WriteLine("Podaj masę w kg: ");
+            masa = float.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj wzrost w metrach: ");
+            wzrost = float.Parse(Console.ReadLine());
+
+            bmi = masa / wzrost;
+            Console.WriteLine("Twój wskaźnik BMI wynosi: {0:F2}", bmi);
+        }
+
+    }
+}
